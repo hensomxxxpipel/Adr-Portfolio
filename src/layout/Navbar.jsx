c7f994360d4d19631760c41a16de4582fa20d230
hensomxxxpipel <rachadrisyh@gmail.com>
2026-02-09 16:07:02 +0700

    update package & change profile photo

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/Button";
 import { Menu, X } from "lucide-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 
 const navLinks = [
@@ -13,9 +13,29 @@ const navLinks = [
 export const Navbar = () => {
 
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      // if (window.scrollY > 50) {
+      //   setIsScrolled(true)
+      // } else {
+      //   setIsScrolled(false)
+      // }
+
+      setIsScrolled(window.scrollY > 50);
+
+    }
+
+    window.addEventListener("scroll", handleScroll)
+
+    return () => window.removeEventListener("scoll", handleScroll)
+  }, []);
 
   return (
-    <header className="fixed top-0 left-0 right-0 bg-transparent py-5 z-50">
+    <header className={`fixed top-0 left-0 right-0 transition-all duration-500 ${
+      isScrolled ? "glass-strong py-3" : "bg-transparent py-5"
+    }  z-50`}>
       <nav className="container mx-auto px-6 flex items-center justify-between">
         <a href="#" className="text-xl font-bold tracking-tight hover:text-primary">
           PM<span className="text-primary">.</span>
